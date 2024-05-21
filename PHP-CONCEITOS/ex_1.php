c8fbc05eaf2e0d6c23f04087278cb08fd89e0b4b
guilhermenou <guilherme123zaz@gmail.com>
2024-05-21 14:59:54 -0300

    exercicio 1 feito

---
+++ PHP-CONCEITOS/ex_1.php
@@ -0,0 +1,12 @@
+<?php 
+
+    echo "digite um número"; //Digita a informação
+    $num1 = trim(fgets(STDIN)); //Lê a informação e a armazena na váriavel $num1
+    echo "digite um número"; 
+    $num2 = trim(fgets(STDIN)); 
+    $num1 + $num2 = $soma;
+    echo "o valor da soma é: $soma";
+
+
+
+
