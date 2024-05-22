417424f1ca3fa216c5ce8269120c8e4eb8285b49
guilhermenou <guilherme123zaz@gmail.com>
2024-05-22 14:08:32 -0300

    exercicio 1,2,3,4 feitos

--- PHP-CONCEITOS/ex_1.php
+++ PHP-CONCEITOS/ex_1.php
@@ -1,12 +1,7 @@
 <?php 
-
-    echo "digite um número"; //Digita a informação
-    $num1 = trim(fgets(STDIN)); //Lê a informação e a armazena na váriavel $num1
+    echo "digite um número"; //escreva
+    $num1 = trim(fgets(STDIN)); //leia
     echo "digite um número"; 
     $num2 = trim(fgets(STDIN)); 
     $num1 + $num2 = $soma;
     echo "o valor da soma é: $soma";
-
-
-
-
--- PHP-CONCEITOS/ex_2.php
+++ PHP-CONCEITOS/ex_2.php
@@ -1,3 +1,10 @@
-<?php 
+<?php	
+    echo"Primeira Nota: ";
+	$n1 = trim(fgets(STDIN));
+    echo"Segunda Nota: ";
+	$n2 = trim(fgets(STDIN));
+    echo"Terceira Nota";
+	$n3 = trim(fgets(STDIN));
 
-echo"";
+	$media=($n1+$n2+$n3)/3;
+	echo"A média é: ";
