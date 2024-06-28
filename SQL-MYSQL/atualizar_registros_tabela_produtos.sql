fa9499ad129899a2792d790fa605d57662e2bc77
Guilherme Augusto <guilherme123zaz@gmail.com>
2024-06-28 14:25:01 -0300

    Acrescentado o arquivo atualizar_registros com alteraçoes feitas

---
+++ SQL-MYSQL/atualizar_registros_tabela_produtos.sql
@@ -0,0 +1,12 @@
+UPDATE registros
+SET preco = 6.10, peso = 3.0
+WHERE sku = 'A1'
+
+UPDATE registros
+SET quantidade_estoque = 100
+WHERE sku = 'A3'
+
+UPDATE registros
+SET unidade_medida = 'g', peso = 80
+WHERE sku = 'A8'
+
