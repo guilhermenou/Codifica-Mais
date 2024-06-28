3584e919356c032114c41fb49950f1090a17652c
Guilherme Augusto <guilherme123zaz@gmail.com>
2024-06-28 13:57:35 -0300

    Exercício da aula 27-06-24 feito criar banco e inserir-registros

--- SQL-MYSQL/criar_banco_produtos.sql
+++ SQL-MYSQL/criar_banco_produtos.sql
@@ -1,18 +1,18 @@
 CREATE DATABASE produtos;
 USE produtos;
 CREATE TABLE produtos(
-id int AUTO_INCREMENT,
+id int AUTO_INCREMENT PRIMARY KEY,
 nome varchar(30),
 sku bigint unique,
 descricao text,
-categoria varchar(30),
+categoria varchar(40),
 preco decimal(10,2),
 unidade_medidda varchar(5),
-peso decimal(6),
+peso decimal(6,2),
 quantidade_estoque bigint(200),
 fabricante varchar(40),
 fornecedor varchar(30),
-deleted_at varchar(10),
-created_at varchar(10),
-updated_at varchar(10),
+deleted_at datetime(10),
+created_at datetime(10),
+updated_at datetime(10),
 );
--- SQL-MYSQL/inserir_registros_tabela_produtos.sql
+++ SQL-MYSQL/inserir_registros_tabela_produtos.sql
@@ -1,24 +1,13 @@
 INSERT INTO registros 
-    (
-    nome, 
-    sku, 
-    descricao, 
-    categoria, 
-    preco, 
-    unidade_medidda, 
-    peso, 
-    quantidade_estoque, 
-    fabricante, 
-    fornecedor) 
-VALUES ("Arroz", "A1","arroz branco", "alimento","5", "kg", "2,0", "150", "sepé", "Devens"),
-       ("Feijão", "A2","Feijão Preto", "alimento","5,80", "kg", "2,5", "120", "Feijó", "Multshow"),
-       ("Macarrão", "A3","arroz branco", "alimento","5", "kg", "2,0", "80", "sepé", "Devens"),
-       ("Maçã", "A4","arroz branco", "alimento","5", "kg", "2,0", "150", "sepé", "Devens"),
-       ("Coca-cola", "A5","arroz branco", "alimento","5", "kg", "2,0", "150", "sepé", "Devens"),
-       ("Água", "A6","arroz branco", "alimento","5", "kg", "2,0", "150", "sepé", "Devens"),
-       ("Biscoito de sal", "A7","arroz branco", "alimento","5", "kg", "2,0", "150", "sepé", "Devens"),
-       ("Achocolatado", "A8","arroz branco", "alimento","5", "kg", "2,0", "150", "sepé", "Devens"),
-       ("Café", "A9","arroz branco", "alimento","5", "kg", "2,0", "150", "sepé", "Devens"),
-       ("Leite", "A10","arroz branco", "alimento","5", "kg", "2,0", "150", "sepé", "Devens"),
-
-;
+    (nome, sku, descricao, categoria, preco, unidade_medida, peso, quantidade_estoque, fabricante, fornecedor) 
+VALUES 
+    ("Arroz", "A1", "Arroz Integral", "alimentos", 4.50, "kg", 2.0, 150, "Sepé", "MegaG"),
+    ("Feijão", "A2", "Feijão Preto", "alimentos", 5.80, "kg", 2.5, 120, "Feijó", "MegaG"),
+    ("Macarrão", "A3", "Macarrão Parafuso", "alimentos", 5.20, "kg", 2.0, 80, "Sepé", "MegaG"),
+    ("Maçã", "A4", "Maçã Verde", "alimentos", 1.50, "g", 2.0, 75, "Fazenda", "Feira-Central"),
+    ("Tomate", "A5", "Tomate Vermelho", "alimentos", 1.80, "kg", 2.0, 40, "Fazenda", "Feira-Central"),
+    ("Refrigerante", "A6", "Coca-Cola", "bebidas", 6.50, "L", 2.0, 155, "Coca Cola", "Coca Cola"),
+    ("Água", "A7", "Água Mineral", "bebidas", 1.00, "kg", 2.0, 110, "Campinho", "Campinho Distribuidora"),
+    ("Biscoito", "A8", "Biscoito Recheado Passatempo", "alimentos", 5.00, "kg", 2.0, 70, "Passa Tempo", "MegaG"),
+    ("Café", "A9", "Café", "alimentos", 5.00, "kg", 2.0, 180, "3 Corações", "MegaG"),
+    ("Leite", "A10", "Leite", "bebidas", 5.00, "L", 2.0, 100, "CAPEL", "MegaG");
