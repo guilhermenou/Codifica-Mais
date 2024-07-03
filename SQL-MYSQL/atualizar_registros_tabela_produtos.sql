25c9272215b6656c5683caa55c040097a541f01c
Guilherme Augusto <guilherme123zaz@gmail.com>
2024-07-03 09:32:04 -0300

    adicionado a funcionalidade NOW()

--- SQL-MYSQL/atualizar_registros_tabela_produtos.sql
+++ SQL-MYSQL/atualizar_registros_tabela_produtos.sql
@@ -1,12 +1,12 @@
 UPDATE registros
-SET preco = 6.10, peso = 3.0
+SET preco = 6.10, peso = 3.0 updated_at = NOW()
 WHERE sku = 'A1'
 
 UPDATE registros
-SET quantidade_estoque = 100
+SET quantidade_estoque = 100 updated_at = NOW()
 WHERE sku = 'A3'
 
 UPDATE registros
-SET unidade_medida = 'g', peso = 80
+SET unidade_medida = 'g', peso = 80 updated_at = NOW()
 WHERE sku = 'A8'
 
